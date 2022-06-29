efda03b91f6dd1b03a1ee369b37d341f07381f1d
Alexandre Silva <>
2022-06-29 18:28:26 -0300

    Adicionando projeto realizado do relógio

---
+++ js/script.js
@@ -0,0 +1,51 @@
+const hourEl = document.getElementById('hour');
+const minuteEl = document.getElementById('minute');
+const secondEl = document.getElementById('second');
+const timeEl = document.getElementById('time');
+const dateEl = document.getElementById('date');
+const toggle = document.getElementById('toggle');
+
+
+const days = ["Domingo", "Segunda-Feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
+const months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+
+toggle.addEventListener('click', (e) => {
+    const html = document.querySelector('html')
+    if (html.classList.contains('dark')) {
+        html.classList.remove('dark')
+        e.target.innerHTML = 'Dark mode'
+    } else {
+        html.classList.add('dark')
+        e.target.innerHTML = 'Light mode'
+    }
+});
+
+
+function setTime() {
+    const time = new Date();
+    const month = time.getMonth();
+    const day = time.getDay();
+    const date = time.getDate();
+    const hour = time.getHours();
+    const hoursForClock = hour % 12;
+    const minute = time.getMinutes();
+    const second = time.getSeconds();
+    const ampm = hour >= 12 ? 'PM' : 'AM';
+
+
+    hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`;
+    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minute, 0, 59, 0, 360)}deg)`;
+    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(second, 0, 59, 0, 360)}deg)`;
+
+    timeEl.innerHTML = `${hoursForClock}:${minute < 10 ? `0${minute}` : minute} ${ampm}`
+    dateEl.innerHTML = `<span class="circle"> ${date}</span>${months[month]}, ${days[day]}`
+}
+
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+setTime()
+
+setInterval(setTime, 1000)
